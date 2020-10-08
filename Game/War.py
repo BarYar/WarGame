1035e4880d429bd94ec491dd3b1ca38162848968
97252 <baryar102030@gmail.com>
2020-10-08 14:12:09 +0300

    8/10/20 14:07 War card game

--- Game/War.py
+++ Game/War.py
@@ -51,6 +51,7 @@ class war:
                 #@gmail.com
         self.newWindow()
         self.newround()
+    #מהלך של שחקן
     def pmove(self,player,card):
         self.discards.append([player,card])
         if(self.countcards<4):
